b1ff810743368110856953a99365e5e4c8bad234
ornitorrinco-espacial <guilherme-chaves@alu.ufc.br>
2018-11-21 14:02:31 -0300

    Correção de bugs
    Bugs corrigidos e posicionamentos no mapa

--- app.js
+++ app.js
@@ -18,21 +18,21 @@ var vm = new Vue({
 			{
 				key: 'dado1',
 				arrValor: 0,
-				texto: 'A agricultura familiar representa 84,4% dos estabelecimentos agrícolas do país, ocupando uma área equivalente a mais de 4 milhões de estádios do Maracanã! (80,25 milhões de hectares)',
+				texto: 'A agricultura familiar representa 84,4% dos estabelecimentos agrícolas do país, ocupando uma área equivalente a mais de 4 milhões de estádios do Maracanã! (80,25 milhões de hectares)<br>Além disso, mais de 74% dos postos de trabalhos rurais estão sob regime de agricultura familiar.',
 				imageSrc: 'img/icons/casa 1.png',
 				imageAlt: 'Agricultura familiar'
 			},
 			{
 				key: 'dado2',
 				arrValor: 1,
-				texto: 'A produção de café na região Sudeste foi a mais importante atividade economica do Brasil no início do século XX, correspondendo a mais de 30% do PIB nacional na época. Contudo, as duas grandes guerras e revoluções que ocorreram em diversos países causou uma grande queda em sua produção, até mesmo ao ponto de queimarem café e jogarem sacas no mar para não causar uma desvalorização excessiva do produto.',
-				imageSrc: 'img/icons/café.png',
+				texto: 'A agricultura familiar é responsável por garantir boa parte da segurança alimentar do País, como importante fornecedora de alimentos para o mercado interno. <br>Mais de 80% da mandioca, 70% do feijão e quase 70% da prdução de leite vêm de estabelecimentos de agricultura familiar, além dela possuir participação importante em outros produtos.',
+				imageSrc: 'img/icons/saco.png',
 				imageAlt: 'Produção de café'
 			},
 			{
 				key: 'dado3',
 				arrValor: 2,
-				texto: 'A produção de café na região Sudeste foi a mais importante atividade economica do Brasil no início do século XX, correspondendo a mais de 30% do PIB nacional na época. Contudo, as duas grandes guerras e revoluções que ocorreram em diversos países causou uma grande queda em sua produção, até mesmo ao ponto de queimarem café e jogarem sacas no mar para não causar uma desvalorização excessiva do produto.',
+				texto: '30% do valor repassado pelo Programa Nacional de Alimentação Escolar (PNAE), é investido na compra direta de produtos da agricultura familiar, medida que estimula o desenvolvimento econômico e sustentável das comunidades.',
 				imageSrc: 'img/icons/café.png',
 				imageAlt: 'Produção de café'
 			}
@@ -190,71 +190,119 @@ var vm = new Vue({
 			no: [
 					{
 						key: 'no00',
-						classe: '',
-						posicao: '',
-						infoIcone: ''
+						classe: 'mandioca',
+						posicao: 'left: 30%; top: 50%;',
+						infoIcone: 'A região é a líder em produção de mandioca no país, com aproximadamente 38% da produção nacional saindo dessa. O estado do Pará é o maior produtor de mandioca do Brasil. No ano de 2017 foram colhidos mais de 4 milhões de toneladas.'
+					},
+					{
+						key: 'no01',
+						classe: 'vaca',
+						posicao: 'left: 70%; top: 20%;',
+						infoIcone: 'O estado do Pará possui o quinto maior rebanho do Brasil, com mais de 20 milhões de cabeças de gado.'
 					}
 				],
 			ne: [
 					{
-						key: '',
-						classe: '',
-						posicao: '',
-						infoIcone: ''
+						key: 'ne00',
+						classe: 'casa1',
+						posicao: 'left: 30%; top: 30%;',
+						infoIcone: 'Aproximadamente 50% dos estabelecimentos que se caracterizam como pertencentes a agricultura familiar estão no Nordeste. São mais de 2 milhões de hectares plantados na região.'
+					},
+					{
+						key: 'ne01',
+						classe: 'saco_feijao',
+						posicao: 'left: 60%; top: 24%;',
+						infoIcone: 'Apesar de possuir a maior área plantada, com mais de 1,5 milhão de hectares plantados, a região Nordeste fica em quarto lugar na produção do grão, com quase 650 mil toneladas do grão colhidas, devido ao baixo rendimento do plantio na região.'
 					}
 				],
 			co: [
 					{
 						key: 'co00',
 						classe: 'soja',
-						posicao: 'left: 10%; top: 15%;',
-						infoIcone: 'A região Centro-Oeste do país é responsável por quase metade da produção de soja nacional, produzindo mais de 50 milhões de toneladas do grão. (Fonte: Conab)'
+						posicao: 'left: 27%; top: 37%;',
+						infoIcone: 'A região Centro-Oeste produz quase 50% de toda a safra da soja do país, com o estado de Mato Grosso sendo o maior produtor do país. Mais de 50 milhões de toneladas do grão são colhidas anualmente.'
 					},
 					{
 						key: 'co01',
 						classe: 'vaca',
-						posicao: 'left: 12%; top: 12%;',
+						posicao: 'left: 48%; top: 14%;',
 						infoIcone: 'Os três estados da região estão entre os cinco maiores detentores de rebanho bovino do país, com Mato Grosso possuindo quase 15% do total nacional.'
 					},
 					{
 						key: 'co02',
 						classe: 'casa2',
-						posicao: 'left: 50%; top: 50%;',
-						infoIcone: 'A região Centro-Oeste possui a maior disparidade de área entre a agricultura familiar e a não-familiar. A última ocupa uma área 10 vezes maior, com aproximadamente 96 milhões de hectares.'
+						posicao: 'left: 52%; top: 45%;',
+						infoIcone: 'A região Centro-Oeste possui a maior disparidade de área entre a agricultura familiar e a não-familiar. A última ocupa uma área 10 vezes maior, com aproximadamente 96 milhões de hectares (área equivalente a 10 vezes o estado de Santa Catarina).'
 					},
 					{
 						key: 'co03',
-						classe: 'soja',
+						classe: 'saco_milho',
 						posicao: 'left: 64%; top: 30%;',
-						infoIcone: 'A região Centro-Oeste do país é responsável por quase metade da produção de soja nacional, produzindo mais de 50 milhões de toneladas do grão. (Fonte: Conab)'
-					},
-					{
-						key: 'co04',
-						classe: 'soja',
-						posicao: 'left: 15%; top: 60%;',
-						infoIcone: 'A região Centro-Oeste do país é responsável por quase metade da produção de soja nacional, produzindo mais de 50 milhões de toneladas do grão. (Fonte: Conab)'
-					},
-					{
-						key: 'co05',
-						classe: 'soja',
-						posicao: 'left: 25%; top: 27%;',
-						infoIcone: 'A região Centro-Oeste do país é responsável por quase metade da produção de soja nacional, produzindo mais de 50 milhões de toneladas do grão. (Fonte: Conab)'
+						infoIcone: 'A região Centro-Oeste é a maior produtora de milho do país, colhendo aproximadamente metade de todo o milho produzido no Brasil, estimado em 90 milhões de toneladas na última safra.'
 					}
 				],
 			se: [
 					{
-						key: '',
-						classe: '',
-						posicao: '',
-						infoIcone: ''
+						key: 'se00',
+						classe: 'saco_cafe',
+						posicao: 'left: 50%; top: 30%;',
+						infoIcone: 'A região Sudeste é detentora de grande parte da produção de café no país, assim como é desde o período imperial. Ela sozinha possui mais de 85% de todas as áreas de plantio do país, que são estimadas em 2,2 milhões de hectares, o equivalente ao estado de Sergipe'
+					},
+					{
+						key: 'se01',
+						classe: 'vaca',
+						posicao: 'left: 40%; top: 64%;',
+						infoIcone: 'O estado de Minas Gerais é o segundo estado brasileiro com relação ao rebanho bovino, com mais de 23 milhões de cabeças de gado, perdendo apenas para o Mato Grosso.'
+					},
+					{
+						key: 'se01',
+						classe: 'leite',
+						posicao: 'left: 60%; top: 20%;',
+						infoIcone: 'A região Sudeste é a maior produtora de leite do país, sendo responsável por aproximadamente 40% da produção de leite nacional.'
 					}
 				],
 			sul: [
 					{
-						key: '',
-						classe: '',
-						posicao: '',
-						infoIcone: ''
+						key: 'su00',
+						classe: 'saco',
+						posicao: 'left: 50%; top: 55%;',
+						infoIcone: 'A região Sul, sozinha, produz 4 vezes mais arroz que todas as outras regiões do país combinadas, com um total de mais de 9 milhões de toneladas do grão nas últimas safras.'
+					},
+					{
+						key: 'su01',
+						classe: 'mandioca',
+						posicao: 'left: 60%; top: 42%',
+						infoIcone: 'Mesmo possuindo uma área de plantação reduzida, o estado do Paraná é o segundo maior produtor de mandioca do país, perdendo apenas para o Pará. Foram colhidos mais de 3 milhões de toneladas no ano de 2017, o equivalente a mais de 3 milhões de carros populares juntos!'
+					},
+					{
+						key: 'su02',
+						classe: 'leite',
+						posicao: 'left: 60%; top: 45%',
+						infoIcone: 'Logo atrás da região Sudeste, a região Sul é a segunda maior produtora de leite do país.'
+					},
+					{
+						key: 'su03',
+						classe: 'soja',
+						posicao: 'left: 55%; top: 20%',
+						infoIcone: 'Favorecida pelo clima regular, a região Sul é a segunda maior produtora de soja do país, com aproximadamente 30% de toda a soja colhida no país vindo da região. Os estados do Paraná e Rio Grande do Sul são, respectivamente, o segundo e o terceiro maior produtor do país, perdendo apenas para Mato Grosso.'
+					},
+					{
+						key: 'su04',
+						classe: 'trigo',
+						posicao: 'left: 40%; top: 35%',
+						infoIcone: 'Devido ao clima ameno, propício a produção de trigo, a região Sul é a maior produtora do grão no país, com aproximadamente 85% de toda a produção nacional vindo dos seus três estados. O estado do Paraná é o maior produtor e detentor da maior área de plantio do grão no Brasil, possuindo quase 50% de toda a área plantada.'
+					},
+					{
+						key: 'su04',
+						classe: 'feijao',
+						posicao: 'left: 41%; top: 28%',
+						infoIcone: 'A região Sul é a maior produtora de feijão do país, detendo mais de 25% da produção nacional, com as região Sudeste, Centro-Oeste e Nordeste produzindo quantidades muito próximas também.'
+					},
+					{
+						key: 'su04',
+						classe: 'galinha',
+						posicao: 'left: 52%; top: 24%',
+						infoIcone: 'Os três estados do Sul são os maiores produtores de frango do país produzindo aproximadamente 8 milhões de toneladas da carne, tanto para o mercado interno como para o externo.'
 					}
 				]
 		},
@@ -285,13 +333,13 @@ var vm = new Vue({
 			},
 			{
 				key: 'fala4',
-				texto: 'A sua direita, temos um mapa do Brasil divido por regiões',
+				texto: 'A sua direita, temos um mapa do Brasil divido por regiões.',
 				tempo: 1500,//ms,
 				styles: 'animation: fala4 4s linear both; animation-iteration-count: 1;'
 			},
 			{
 				key: 'fala5',
-				texto: 'Por que não tenta navegar pelas regiões?',
+				texto: 'Por que não tenta navegar por elas?',
 				tempo: 1500,//ms,
 				styles: 'animation: fala5 3.2s linear both; animation-iteration-count: 1;'
 			},
@@ -403,9 +451,9 @@ var vm = new Vue({
 	computed: {
 		classeMapaR: function(){
 			if(this.status == 2) {
-				return 'col-sm-12';
+				return 'col-sm-10';
 			}
-			return 'col-sm-7';
+			return 'col-sm-6';
 		}
 	},
 	mounted: function () {
